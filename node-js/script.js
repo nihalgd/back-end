cc1a4649a8c9845b7b5d7b482f4ed8b816a53fa1
nihalgd <nihalgoud.12345@gmail.com>
2026-03-29 18:26:41 +0530

    Learn Back-end

--- node-js/script.js
+++ node-js/script.js
@@ -1,8 +1,10 @@
-let a = "nihal"
-console.log(a);
+// let a = "nihal"
+// console.log(a);
 
 // let args = process.argv;
 
 // for (let i = 0; i < argv.length; i++) {
 //     console.log("hellow" , args[i]);
 // }
+
+const fs = require('fs')
