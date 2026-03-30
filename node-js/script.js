395f9183a7663078e961274aa9a4c1fa9959c935
nihalgd <nihalgoud.12345@gmail.com>
2026-03-30 16:57:49 +0530

    Learn Back-end

--- node-js/script.js
+++ node-js/script.js
@@ -7,4 +7,46 @@
 //     console.log("hellow" , args[i]);
 // }
 
+
+// fs in node js - - - - - - - - - - - - - - - - - - - - - - - - - - - - fs in node js - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+const { log } = require('console')
 const fs = require('fs')
+
+// fs.writeFile("heyy.txt" , "hellooww kaise hoo" , function(err){
+//     if(err) console.log(err);
+//     else console.log("done");
+// })
+
+// fs.appendFile("heyy.txt" , "badiyaa" , function(err){
+//     if(err) console.log(err);
+//     else console.log("done");
+// })
+
+// fs.rename("heyy.txt" , "hellow.txt", function(err){
+//     if(err) console.log(err);
+//     else console.log("do");  
+// } )
+
+// fs.copyFile("hellow.txt" , "hellow1.txt" , function(err){
+//     if(err) console.log(err.message);
+//     else console.log("done");
+// })
+
+// fs.unlink("hellow1.txt" , function(err){
+//     if(err) console.log(err.message);
+//     else console.log("file delete");  
+// })
+
+// fs.rm("./package-lock.json", {recursive : true} , function(err){
+//     if(err) console.log(err.message);
+//     else console.log("folder removed");
+// })
+
+// HTTP - HTTPS - - - - - - - - - - - - - - - - HTTP - HTTPS - - - - - - - - - - - - - - - - HTTP - HTTPS - - - - - - - -
+
+// const http = require('node:http')
+// const server = http.createServer(function(req,res){
+//     res.end("hellow world");
+// })
+// server.listen(3000);
