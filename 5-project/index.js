36f56753192a3adb0d977da715075b2f31b18dca
nihalgd <nihalgoud.12345@gmail.com>
2026-04-04 17:09:07 +0530

    Learn Back-end

--- 5-project/index.js
+++ 5-project/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -8,8 +9,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine' , 'ejs');
 
 app.get("/" , function(req , res){
-    res.render("index")
+    fs.readdir(`./files`, function(err,files){
+        console.log(files);
+        res.render("index" , {files:files});  
+    })
 });
+
+app.post("/create" , function(req , res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt` , req.body.details,function(err){
+        res.redirect("/")
+    })
+})
 
 
 app.listen(3000 , function (){
