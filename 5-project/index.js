b0d49c6d4ec4c5edcb433985982925097a132ec8
nihalgd <nihalgoud.12345@gmail.com>
2026-04-04 14:50:57 +0530

    Learn Back-end

--- 5-project/index.js
+++ 5-project/index.js
@@ -1,1 +1,18 @@
-console.log("nihaalll goud");
+const express = require('express')
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine' , 'ejs');
+
+app.get("/" , function(req , res){
+    res.render("index")
+});
+
+
+app.listen(3000 , function (){
+    console.log("its fuckingg runnning!");
+});
+
