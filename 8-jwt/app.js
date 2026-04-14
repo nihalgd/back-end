3d7a596ac385a2bce81a3c2c75224c7212f0e00b
nihalgd <nihalgoud.12345@gmail.com>
2026-04-14 23:22:37 +0530

    Learn Back-end

--- 8-jwt/app.js
+++ 8-jwt/app.js
@@ -1,15 +1,20 @@
 const cookieParser = require('cookie-parser');
 const express = require('express')
 const app = express();
-app.use(cookieParser);
+app.use(cookieParser());
+const bcrypt = require('bcrypt');
 
 app.get("/", function (req, res) {
     res.cookie("name", "nihal")
     res.send("heeyyyyyy!!")
-})
-
-app.get("/" , function(req , res) {
     console.log(req.cookies);
 })
+
+bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+        // Store hash in your password DB.
+    });
+});
+
 
 app.listen(3000);
