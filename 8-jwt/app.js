1b907ab57b3217e934987c25b0aa365ea01ecba8
nihalgd <nihalgoud.12345@gmail.com>
2026-04-14 23:00:15 +0530

    Learn Back-end

--- 8-jwt/app.js
+++ 8-jwt/app.js
@@ -1,9 +1,15 @@
+const cookieParser = require('cookie-parser');
 const express = require('express')
 const app = express();
+app.use(cookieParser);
 
 app.get("/", function (req, res) {
     res.cookie("name", "nihal")
     res.send("heeyyyyyy!!")
+})
+
+app.get("/" , function(req , res) {
+    console.log(req.cookies);
 })
 
 app.listen(3000);
