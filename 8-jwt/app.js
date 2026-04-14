6a368a4393f829fb70ee7f5288f994c5b4e201f2
nihalgd <nihalgoud.12345@gmail.com>
2026-04-15 00:04:36 +0530

    Learn Back-end

--- 8-jwt/app.js
+++ 8-jwt/app.js
@@ -3,23 +3,35 @@ const express = require('express')
 const app = express();
 app.use(cookieParser());
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 app.get("/", function (req, res) {
 
-    res.cookie("name", "nihal-goud")
-    console.log(req.cookies);
+    let token = jwt.sign({email: "nihal@gmail.com"} , "secret")
+    let data = jwt.verify(req.cookies.token ,"secret")
 
-    bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash("nihal-goud", salt, function(err, hash) {
-          console.log(hash);  
-        });
-    });
+    console.log(data);
+    
 
-    bcrypt.compare("nihal-goud", "$2b$10$.HQNgxlZRtLIohCCyS0tAOyPY0LCWxO0OAAunR5x3XLzuDBddn6uW", function(err, result) {
-        res.send("heeyyyyyy!!")
-        console.log(result);
-    // result == true
-    });
+    res.cookie("token" , token)
+    res.send("OK!")
+
+
+
+
+//     res.cookie("name", "nihal-goud")
+//     console.log(req.cookies);
+
+//     bcrypt.genSalt(10, function(err, salt) {
+//     bcrypt.hash("nihal-goud", salt, function(err, hash) {
+//           console.log(hash);  
+//         });
+//     });
+
+//     bcrypt.compare("nihal-goud", "$2b$10$.HQNgxlZRtLIohCCyS0tAOyPY0LCWxO0OAAunR5x3XLzuDBddn6uW", function(err, result) {
+//         res.send("heeyyyyyy!!")
+//         console.log(result);
+//     });
 })
 
 
